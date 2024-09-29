c9c42b6d308404125fb55c8dd6005965a83ad4ca
GeorgeDanicico <georgedanicico@yahoo.com>
2024-09-29 22:14:07 +0300

    Update

--- frontend/src/components/user-active-reservations/UserActiveReservation.tsx
+++ frontend/src/components/user-active-reservations/UserActiveReservation.tsx
@@ -6,6 +6,7 @@ import axios from '@/utils/axios';
 import { convertDate } from '@/utils/utils';
 import toast from 'react-hot-toast';
 import ConfirmationModal from '@/utils/ConfirmationModal';
+import { Button } from '@nextui-org/react';
 
 const UserActiveReservations = () => {
     const { userDetails, removeReservation } = useStore();
@@ -42,13 +43,13 @@ const UserActiveReservations = () => {
             <div className="flex flex-col w-full">
                 {userDetails?.activeReservations?.length === 0 && <span className='text-2xl'>Nu ai nicio rezervare activa</span>}
                 {userDetails?.activeReservations?.map((reservation: any) => (
-                    <div key={reservation.reservationId} className='grid grid-cols-2 gap-2 border-2 p-2 border-primary'>
+                    <div key={reservation.reservationId} className='grid grid-cols-2 gap-2 bg-lightGray p-2 rounded'>
                         <span>{convertDate(reservation.date, true)}</span>
                         <span>{reservation.sessionName}</span>
                         <span>{reservation.coachName}</span>
                         <span className='font-bold text-lg'>REZERVAT</span>
-                        <button className='col-span-2 text-white bg-primary font-semibold text-lg'
-                            onClick={() => openConfirmationModal(reservation.reservationId)}>Renunta</button>
+                        <Button className='col-span-2 text-white bg-primary font-semibold text-lg'
+                            onClick={() => openConfirmationModal(reservation.reservationId)}>Renunta</Button>
                     </div>
                 ))}
                 <ConfirmationModal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)} onConfirm={() => handleCancelReservation()}/>
